17b82bdd9f10ac6be20971ab6eca6a48eec229e2
yangy <yangyuan@imudges.com>
2016-10-12 19:15:33 +0800

    Initial commit

---
+++ src/com/example/CircleSmartImageView/MyActivity.java
@@ -0,0 +1,18 @@
+package com.example.CircleSmartImageView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        CircleSmartImageView test=(CircleSmartImageView) findViewById(R.id.test);
+        test.setImageUrl("http://b.hiphotos.baidu.com/baike/w%3D268%3Bg%3D0/sign=92e00c9b8f5494ee8722081f15ce87c3/29381f30e924b899c83ff41c6d061d950a7bf697.jpg");
+
+    }
+}
